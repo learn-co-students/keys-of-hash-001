87d0b8a9097c69a9a20154b13bb5cfa86e9f69a0
lwadya <lukaswadya@yahoo.com>
2015-10-11 22:17:42 -0400

    Finished method

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    output = arguments.collect do |value|
+      self.keys.select do |key|
+        self[key] == value
+      end
+    end
+    output.flatten
   end
 end
