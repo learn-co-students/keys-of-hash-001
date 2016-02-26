8f2e5cf542e929c449930da4fa6eab5a331f80d3
thaik1618 <kthai1618@gmail.com>
2016-02-26 23:39:38 +0100

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,22 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(hash,*values)
+    array = []
+    hash.each do |k,v|
+      values.each do |value|
+        if v == value
+          array.push(k)
+        else
+          nil
+        end
+      end
+    end
+    array
   end
 end
+
+char = {"me" => 3, "soph" => 4, "dad" => "5"}
+puts char.keys_of(7,3,2)
+
+
+
+
